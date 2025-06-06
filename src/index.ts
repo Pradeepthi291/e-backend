99a2b576c00678c6ea01e41acf2bea66a86677b7
Pradeepthi291 <pradeepthi291@gmail.com>
2025-06-06 18:24:57 +0530

    local to deploy links

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://e-frontend-omega.vercel.app/",
     credentials: true,
   })
 );
